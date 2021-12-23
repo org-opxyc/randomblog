ded650cdf919503a324f405b0792e6849a9ec649
Bony Emmanuel <95271796+be-v@users.noreply.github.com>
2021-12-23 13:17:09 +0530

    Add pages for english and japanese blogs

---
+++ src/components/navigation.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import * as styles from './navigation.module.css'
+
+const Navigation = () => (
+  <nav role="navigation" className={styles.container} aria-label="Main">
+    <Link to="/" className={styles.logoLink}>
+      <span className={styles.logo} />
+      <span className={styles.navigationItem}>Gatsby Starter Contentful</span>
+    </Link>
+    <ul className={styles.navigation}>
+      <li className={styles.navigationItem}>
+        <Link to="/" activeClassName="active">
+          Home
+        </Link>
+      </li>
+      <li className={styles.navigationItem}>
+        <Link to="/en/" activeClassName="active">
+          EN
+        </Link>
+      </li>
+      <li className={styles.navigationItem}>
+        <Link to="/jp/" activeClassName="active">
+          日本
+        </Link>
+      </li>
+    </ul>
+  </nav>
+)
+
+export default Navigation
---
+++ src/pages/jp.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+import get from 'lodash/get'
+
+import Seo from '../components/seo'
+import Layout from '../components/layout'
+import Hero from '../components/hero'
+import ArticlePreview from '../components/article-preview'
+
+class JPIndex extends React.Component {
+  render() {
+    const posts = get(this, 'props.data.allContentfulBlogPost.nodes')
+
+    return (
+      <Layout location={this.props.location}>
+        <Seo title="ブログ" />
+        <Hero title="ブログ" />
+        <ArticlePreview posts={posts} />
+      </Layout>
+    )
+  }
+}
+
+export default JPIndex
+
+export const pageQuery = graphql`
+  query JPIndexQuery {
+    allContentfulBlogPost(filter: {language: {eq: "jp"}}, sort: { fields: [publishDate], order: DESC }) {
+      nodes {
+        title
+        slug
+        publishDate(formatString: "MMMM Do, YYYY")
+        tags
+        heroImage {
+          gatsbyImageData(
+            layout: FULL_WIDTH
+            placeholder: BLURRED
+            width: 424
+            height: 212
+          )
+        }
+        description {
+          childMarkdownRemark {
+            html
+          }
+        }
+        language
+      }
+    }
+  }
+`
