5dd8c319623a8686b95dfd41c2148e752c5817ac
Bony Emmanuel <95271796+be-v@users.noreply.github.com>
2022-01-04 12:11:49 +0530

    Add hubspot chatbot

---
+++ gatsby-ssr.js
@@ -0,0 +1,3 @@
+const React = require("react");
+exports.onRenderBody = ({ setPreBodyComponents }) => {
+setPreBodyComponents([ <script type="text/javascript" key="hubspot-chat" id="hs-script-loader" async defer src="//js-eu1.hs-scripts.com/25431987.js"></script> ]); };
